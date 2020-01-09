6b97d329efb9e91938445ac1f60816a733d86fbd
BlackVS <vvs@coders.in.ua>
2020-01-09 20:31:39 +0200

    bug fixes

--- main/cmd_join.cpp
+++ main/cmd_join.cpp
@@ -17,15 +17,42 @@ static int cmd_join(int argc, char **argv) {
     return 1;
   }
 
-  wifi_join.ssid=join_args.ssid->sval[0];
-  wifi_join.password=join_args.password->sval[0];
+  esp_err_t err=ESP_OK;
+  bool bNewCreds=false;
+
+  if(join_args.ssid->count) {
+    wifi_join.ssid=join_args.ssid->sval[0];
+    bNewCreds=true;
+  }
+  else {
+    err=nvs_settings_getString("wifi-join-ssid", wifi_join.ssid);
+    if(err!=ESP_OK) {
+      printf("Error: no SSID set!!!");
+      return 0;
+    }
+  }
+
+  if(join_args.password->count) {
+    wifi_join.password=join_args.password->sval[0];
+    bNewCreds=true;
+  }
+  else {
+    err=nvs_settings_getString("wifi-join-pass", wifi_join.password);
+    if(err!=ESP_OK) {
+      printf("Warning: no password set");
+      wifi_join.password.clear();
+    }
+  }
+
   wifi_join.timeout=join_args.timeout->ival[0];
 
-  wifi_join.start();
-
-  //nvs_settings_setString("wifi-user-ap", join_args.ssid->sval[0]);
-  //nvs_settings_setString("wifi-user-pass", join_args.password->sval[0]);
-  //printf("WiFi Connected, credentials saved\r\n");
+  err = wifi_join.start();
+  if(err==ESP_OK&&wifi_join.is_connected()&&bNewCreds)
+  {
+    nvs_settings_setString("wifi-join-ssid", wifi_join.ssid.c_str());
+    nvs_settings_setString("wifi-join-pass", wifi_join.password.c_str());
+    printf("Credentials saved\n");
+  }
 
   return 0;
 }
@@ -34,12 +61,12 @@ void register_cmd_join()
 {
   join_args.timeout = arg_int0(NULL, "timeout", "<t>", "Connection timeout, ms");
   join_args.timeout->ival[0] = 15000; // set default value
-  join_args.ssid = arg_str1(NULL, NULL, "<ssid>", "SSID of AP");
+  join_args.ssid = arg_str0(NULL, NULL, "<ssid>", "SSID of AP");
   join_args.password = arg_str0(NULL, NULL, "<pass>", "PSK of AP");
   join_args.end = arg_end(2);
 
   const esp_console_cmd_t join_cmd = {.command = "join",
-                                      .help = "Join WiFi AP as a station",
+                                      .help = "Join to WiFi AP as a station. If no ssid/password set - try use stored in nvs",
                                       .hint = NULL,
                                       .func = &cmd_join,
                                       .argtable = &join_args};
--- main/nvs.cpp
+++ main/nvs.cpp
@@ -48,17 +48,41 @@ int32_t nvs_settings_getI32(const char *name) {
   return value;
 }
 
-void nvs_settings_setString(const char *name, const char *value) {
+esp_err_t nvs_settings_setString(const char *name, const char *value) {
+  esp_err_t err=ESP_OK;
   nvs_handle my_handle;
   nvs_open(NVS_NAMESPACE, NVS_READWRITE, &my_handle);
-  nvs_set_str(my_handle, name, value);
+  err=nvs_set_str(my_handle, name, value);
   nvs_commit(my_handle);
   nvs_close(my_handle);
+  return err;
 }
 
-void nvs_settings_getString(const char *name, char *value, size_t maxLen) {
+esp_err_t nvs_settings_setString(const char *name, std::string  value) 
+{
+  return nvs_settings_setString(name, value.c_str());
+}
+
+esp_err_t nvs_settings_getString(const char *name, char *value, size_t maxLen) {
+  esp_err_t err=ESP_OK;
   nvs_handle my_handle;
   nvs_open(NVS_NAMESPACE, NVS_READONLY, &my_handle);
-  nvs_get_str(my_handle, name, value, &maxLen);
+  err=nvs_get_str(my_handle, name, value, &maxLen);
+  nvs_close(my_handle); 
+  return err;
+}
+
+esp_err_t nvs_settings_getString(const char *name, std::string& value) {
+  esp_err_t err=ESP_OK;
+  nvs_handle my_handle;
+  nvs_open(NVS_NAMESPACE, NVS_READONLY, &my_handle);
+  size_t l=0;  
+  err=nvs_get_str(my_handle, name, NULL, &l);
+  if(err==ESP_OK&&l>0) {
+    value.reserve(l);
+    l=value.capacity();
+    err=nvs_get_str(my_handle, name, &value[0], &l);
+  }
   nvs_close(my_handle);
+  return err;
 }
